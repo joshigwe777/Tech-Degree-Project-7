f773cfe4439811d78c13085d7e13824200ce545a
joshigwe777 <joshigwe777@gmail.com>
2021-07-07 22:00:31 -0400

    line chart

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const alertBanner = document.getElementById("alert");
+const trafficCanvas = document.getElementById("traffic-chart");
 
 // create HTML for the banner
 alertBanner.innerHTML = 
@@ -14,4 +15,42 @@ alertBanner.addEventListener('click', e => {
     if (element.classList.contains("alert-banner-close")) {
         alertBanner.stryle.display = "none";
     }
+});
+
+//traffic chart data object
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+    }]
+}
+
+// traffic chart options object
+
+let trafficOptions = {
+    aspectRation: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+// create chart
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
 });
