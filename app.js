a27e8bbeeace67a3e20c729ba0eeba6bacd2f678
joshigwe777 <joshigwe777@gmail.com>
2021-07-07 21:31:34 -0400

    html and css initial draft

---
+++ app.js
@@ -0,0 +1,17 @@
+const alertBanner = document.getElementById("alert");
+
+// create HTML for the banner
+alertBanner.innerHTML = 
+`<div class="alert-banner">
+    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+    <p class="alert-banner-close">x</p>
+</div>`;
+
+//adding event listener on the alert banner
+
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.stryle.display = "none";
+    }
+});
