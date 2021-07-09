5f135786d01a7a856d12a866e1d0f303348bfd12
joshigwe777 <joshigwe777@gmail.com>
2021-07-09 16:23:23 -0400

    charts added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const alertBanner = document.getElementById("alert");
 const trafficCanvas = document.getElementById("traffic-chart");
+const dailyCanvas = document.getElementById("daily-chart");
+const mobileCanvas = document.getElementById("mobile-chart");
 
 // create HTML for the banner
 alertBanner.innerHTML = 
@@ -17,7 +19,7 @@ alertBanner.addEventListener('click', e => {
     }
 });
 
-//traffic chart data object
+//chart data objects
 
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
@@ -26,9 +28,33 @@ let trafficData = {
         backgroundColor: 'rgba(116, 119, 191, .3)',
         borderWidth: 1,
     }]
-}
+};
 
-// traffic chart options object
+let dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1,
+    }]
+};
+
+let mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '78CF82',
+            '#51B6C8'
+        ]
+    }]
+};
+
+// chart options objects
 
 let trafficOptions = {
     aspectRation: 2.5,
@@ -47,10 +73,49 @@ let trafficOptions = {
     }
 };
 
-// create chart
+let dailyOptions = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+let mobileOptions = {
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxwidth: 20,
+                fontStyle: 'bold'
+            }
+        }
+    }
+};
+
+
+
+// create charts
 
 let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
     data: trafficData,
     options: trafficOptions
+});
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
 });
